7ce0ad372ce717150fbddc24add1a531b32163e6
hyun <nomzaxs@gmail.com>
2022-01-27 23:51:00 +0900

    220127

--- Baekjoon_java/src/simple_math_2/PN_1929.java
+++ Baekjoon_java/src/simple_math_2/PN_1929.java
@@ -23,12 +23,17 @@ M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오
 ***************************************************************************************************/
 
 // 시간 초과됨 n^2을 줄여야 함
+// 에라토스테네시의 체 라는 소수를 찾는 알고리즘.
+// 소수의 곱을 구하려는 수의 제곱근의 배수들만 지워도 충분하다는 알고리즘이다.
 
 package simple_math_2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class PN_1929 {
@@ -38,21 +43,39 @@ public class PN_1929 {
 
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int check;
 
-        for(int i = M; i < N + 1; i++){
-            check = 0;
+        ArrayList<Boolean> primeList;
 
-            for(int j = 2; j < i; j++){
-                if(i % j == 0){
-                    check++;
-                    break;
+        primeList = new ArrayList<Boolean>(N + 1);
+
+        // 0, 1번째를 소수 아님으로 처리
+        primeList.add(false);
+        primeList.add(false);
+
+        // 2부터 소수로 설정
+        for(int i = 2; i < N + 1; i++){
+            primeList.add(i, true);
+        }
+
+        // 소수 판별
+        for(int i = 2; i < primeList.size(); i++){
+            if(primeList.get(i)){
+                for(int j = i * 2; j < N + 1; j += i){
+                    primeList.set(j, false);
                 }
             }
+        }
 
-            if(check == 0){
-                System.out.println(i);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 소수일 시 출력
+        for(int i = M; i < primeList.size(); i++){
+            if(primeList.get(i)){
+                bw.write(i + "\n");
             }
         }
+
+        bw.flush();
+        bw.close();
     }
 }
