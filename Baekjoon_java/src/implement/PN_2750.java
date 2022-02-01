6eaffc7bbfd8f356afc029a765baf9abd6bfc307
hyun <nomzaxs@gmail.com>
2022-02-02 03:02:05 +0900

    220201

---
+++ Baekjoon_java/src/implement/PN_2750.java
@@ -0,0 +1,56 @@
+/***************************************************************************************************
+2750번 수 정렬하기
+
+limit_time : 1s, limit_memory : 128MB
+
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+예제 입력 1 
+5
+5
+2
+3
+4
+1
+예제 출력 1 
+1
+2
+3
+4
+5
+***************************************************************************************************/
+
+package implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PN_2750 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int n = 0; n < N; n++){
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(arr);
+
+        for(int i = 0; i < arr.size(); i++){
+            System.out.println(arr.get(i));
+        }
+    }
+}
