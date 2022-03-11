bc8dd981751f7c78063ed3cf367306cb1a6fcb48
hyun <nomzaxs@gmail.com>
2022-03-11 23:54:01 +0900

    220310

---
+++ Baekjoon_java/src/prefix/PN_11659.java
@@ -0,0 +1,71 @@
+/***************************************************************************************************
+11659번 구간 합 구하기 4
+
+limit_time : 1s, limit_memory : 256MB
+
+문제
+수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+
+출력
+총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+
+제한
+1 ≤ N ≤ 100,000
+1 ≤ M ≤ 100,000
+1 ≤ i ≤ j ≤ N
+예제 입력 1 
+5 3
+5 4 3 2 1
+1 3
+2 4
+5 5
+예제 출력 1 
+12
+9
+1
+***************************************************************************************************/
+
+package prefix;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PN_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        int[] prefix = new int[N + 1];
+
+        arr[0] = 0;
+        prefix[0] = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int n = 1; n <= N; n++) {
+            arr[n] = Integer.parseInt(st.nextToken());
+            prefix[n] = prefix[n - 1] + arr[n];
+        }
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            sb.append(prefix[j] - prefix[i - 1] + "\n");
+        }
+
+        sb.setLength(sb.length() - 1);
+        System.out.println(sb.toString());
+    }
+}
