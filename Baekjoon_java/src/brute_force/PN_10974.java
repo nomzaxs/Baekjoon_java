9e58b410fbbf21e9eadfcd666b7b028d9d52ed1e
nomzaxs <nomzaxs@gmail.com>
2022-06-23 01:30:14 +0900

    10974번 모든 순열

--- Baekjoon_java/src/brute_force/PN_10974.java
+++ Baekjoon_java/src/brute_force/PN_10974.java
@@ -1,3 +1,28 @@
+/***************************************************************************************************
+10974번 모든 순열
+
+limit_time : 1s, limit_memory : 256MB
+
+문제
+N이 주어졌을 때, 1부터 N까지의 수로 이루어진 순열을 사전순으로 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 8)이 주어진다. 
+
+출력
+첫째 줄부터 N!개의 줄에 걸쳐서 모든 순열을 사전순으로 출력한다.
+
+예제 입력 1 
+3
+예제 출력 1 
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+***************************************************************************************************/
+
 package brute_force;
 
 import java.io.BufferedReader;
@@ -5,27 +30,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class PN_10974 {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        arr = new int[n];
+        visited = new boolean[n];
 
-        for(int i = 0; i < N; i++){
-            arr[i] = i + 1;
-        }
-
-        permutation(arr, 0, arr.length);
+        recur(0);
     }
 
-    private static void permutation(int[] arr, int s, int e) {
-        int[] per = new int[e];
+    public static void recur(int cur) {
 
-        if(s == e){
-            for(int i : arr){
-                
+        // 기저 조건
+        if (cur == n) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + 1 + " ");
             }
+            System.out.println();
+            return;
+        }
+
+        // 재귀 호출
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue; // 사용중이면 건너뜀
+            }
+            arr[cur] = i;
+            visited[i] = true; // 사용중인지
+            recur(cur + 1);
+            visited[i] = false; // 출력이 되면 해제
         }
     }
 }
